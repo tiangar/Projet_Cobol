9fe4fbb03625e40522c4f2fb2c2813e47cdf04a8
tiangar <larochema85@gmail.com>
2017-02-28 12:59:51 +0100

    Add files via upload

--- Projet.cob
+++ Projet.cob
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
       ******************************************************************
       * Author:
       * Date:
@@ -49,12 +48,40 @@
            77 fclt_stat PIC 9(2).
 
            77 Wrep PIC 9.
+           77 Wmenu PIC 9.
+           77 Wgre PIC A.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
+           OPEN I-O fdvd
+           IF fdvd_stat =35 THEN
+           OPEN OUTPUT fdvd
+           CLOSE fdvd
+           OPEN I-O fdvd
+           END-IF
 
+           PERFORM MENU
+
+           CLOSE fdvd
+           STOP RUN.
+      ******************************************************************
+      ******************************************************************
+      ******************************************************************
+       MENU.
+       PERFORM WITH TEST AFTER UNTIL Wmenu = 0
+           DISPLAY "1-Ajout DVD"
+           DISPLAY "0-Quitter"
+           ACCEPT Wmenu
+           EVALUATE Wmenu
+           WHEN 0
+           MOVE 0 TO Wmenu
+           WHEN 1
+           PERFORM AJOUT_DVD
+       END-PERFORM.
+      *>-------------------------------------------debut_partie_matthieu
+      ******************************************************************
+      ******************************************************************
+      ******************************************************************
        AJOUT_DVD.
        PERFORM WITH TEST AFTER UNTIL Wrep = 0
            DISPLAY "Donnez les informations du DVD : "
@@ -76,60 +103,22 @@
                ACCEPT Wrep
            END-PERFORM
        END-PERFORM.
-       END PROGRAM Projet.
-=======
       ******************************************************************
-      * Author:
-      * Date:
-      * Purpose:
-      * Tectonics: cobc
       ******************************************************************
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. Projet.
+      ******************************************************************
+       RECHERCHE_DVD.
+           MOVE Wgre TO fbu_ville
+           START fbureau
+               KEY = fbu_ville
+           INVALID KEY
+               DISPLAY "Pas de bureau !!!"
+           NOT INVALID KEY
+               PERFORM
 
-       ENVIRONMENT DIVISION.
-       INPUT-OUTPUT SECTION.
-       FILE-CONTROL.
-           SELECT fdvd ASSIGN TO "fdvd.dat"
-           ORGANIZATION INDEXED
-           ACCESS IS DYNAMIC
-           RECORD KEY fdvd_id
-           ALTERNATE RECORD KEY fdvd_titre WITH DUPLICATES
-           ALTERNATE RECORD KEY fdvd_genre WITH DUPLICATES
-           FILE STATUS IS fdvd_stat.
-
-           SELECT fclients ASSIGN TO "fclients.dat"
-           ORGANIZATION INDEXED
-           ACCESS IS DYNAMIC
-           RECORD KEY fclt_id
-           FILE STATUS IS fclt_stat.
-
-       DATA DIVISION.
-       FILE SECTION.
-           FD fdvd.
-           01 dvdTampon.
-               02 fdvd_id PIC 9(10).
-               02 fdvd_titre PIC A.
-               02 fdvd_real PIC A.
-               02 fdvd_genre PIC A.
-               02 fdvd_louable PIC 9(2).
-               02 fdvd_prix PIC 9.
-
-           FD fclients.
-           01 cltTampon.
-               02 fclt_id PIC 9(10).
-               02 fclt_nom PIC A.
-               02 fclt_prenom PIC A.
-               02 fclt_mail PIC A.
-
-
-       WORKING-STORAGE SECTION.
-           77 fdvd_stat PIC 9(2).
-           77 fclt_stat PIC 9(2).
-
-       PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
+               END-PERFORM
+           END-START.
+      ******************************************************************
+      ******************************************************************
+      ******************************************************************
+      *>---------------------------------------------fin_partie_matthieu
        END PROGRAM Projet.
->>>>>>> origin/master
